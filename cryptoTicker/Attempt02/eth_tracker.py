c4de8dacd5688d35ef00db01cf7ef68ba1c0a48c
robertgorowsky <robert.gorowsky@sportngin.com>
2021-10-13 03:29:44 -0500

    can search for specific coins from api

--- cryptoTicker/Attempt02/eth_tracker.py
+++ cryptoTicker/Attempt02/eth_tracker.py
@@ -18,4 +18,14 @@ url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
 returned_json = requests.get(url, params=params, headers=headers).json()
 
-print(returned_json)
+coin_data = returned_json['data']
+
+search_key = 'ETH'
+for x in coin_data: # trying to print the 
+  key_key = x['symbol']
+  if search_key == key_key:
+    print(key_key, x['quote'])
+
+# this prints the data on the top 5 listed coins (assuming details from params section)
+# for x in coin_data: 
+#   print(x['symbol'], x['quote'])
