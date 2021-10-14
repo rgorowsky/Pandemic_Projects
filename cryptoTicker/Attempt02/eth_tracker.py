23456ead15afbe4b16cdb6bb7c9359fae07c2931
robertgorowsky <robert.gorowsky@sportngin.com>
2021-10-14 00:18:50 -0500

    have it down to pulling current crypto prices by coin key value

--- cryptoTicker/Attempt02/eth_tracker.py
+++ cryptoTicker/Attempt02/eth_tracker.py
@@ -20,12 +20,38 @@ returned_json = requests.get(url, params=params, headers=headers).json()
 
 coin_data = returned_json['data']
 
-search_key = 'ETH'
-for x in coin_data: # trying to print the 
-  key_key = x['symbol']
-  if search_key == key_key:
-    print(key_key, x['quote'])
+# for printing etherum prices
+eth_search_key = 'ETH'
+for x in coin_data:
+  eth_abbrev = x['symbol']
+  eth_price = x['quote']['USD']['price']
+  if eth_search_key == eth_abbrev:
+    print(eth_abbrev, eth_price)
 
-# this prints the data on the top 5 listed coins (assuming details from params section)
-# for x in coin_data: 
-#   print(x['symbol'], x['quote'])
+# for printing bitcoin prices
+btc_search_key = 'BTC'
+for x in coin_data:
+  btc_abbrev = x['symbol']
+  btc_price = x['quote']['USD']['price']
+  if btc_search_key == btc_abbrev:
+    print(btc_abbrev, btc_price)
+
+# for printing binance coin prices
+bnb_search_key = 'BTC'
+for x in coin_data:
+  bnb_abbrev = x['symbol']
+  bnb_price = x['quote']['USD']['price']
+  if bnb_search_key == bnb_abbrev:
+    print(bnb_abbrev, bnb_price)
+
+# for printing cardano coin prices
+ada_search_key = 'ADA'
+for x in coin_data:
+  ada_abbrev = x['symbol']
+  ada_price = x['quote']['USD']['price']
+  if ada_search_key == ada_abbrev:
+    print(ada_abbrev, ada_price)
+
+# print all 5 coins
+# for x in coin_data:
+#   print(x['symbol'], x['quote']['USD']['price'])
